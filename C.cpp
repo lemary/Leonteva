a42de3a0b1b7a94a58df0f71b88935121a7bd3e1
Maria <leonteva.ms@phystech.edu>
2020-11-02 16:29:25 +0300

    new com

---
+++ C.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+
+using std::cin;
+using std::cout;
+
+int partition(std::vector<int> &mas, int l, int r){
+    int id = rand() % (r - l + 1) + l;
+    std::vector<int> buf(r-l+1);
+    int fs = 0;
+    int ls = r-l;
+    for(int i = l; i <= r; ++i){
+        if(i != id){
+            if(mas[i] < mas[id]){
+                buf[fs] = mas[i];
+                fs++;
+            }else{
+                buf[ls] = mas[i];
+                ls--;
+            }
+        }
+    }
+    buf[fs] = mas[id];
+    for(int i = l; i <= r; ++i){
+        mas[i] = buf[i-l];
+    }
+    return l+fs;
+}
+
+int kth(std::vector<int> &mas, int l, int r, int k) {
+    if (l == r) {
+        return mas[l];
+    }
+    int pivotId = partition(mas, l, r);
+    if(pivotId - l > k){
+        return kth(mas, l, pivotId-1, k);
+    }else{
+        return kth(mas, pivotId, r, k - (pivotId - l));
+    }
+}
+int main() {
+    std::ios::sync_with_stdio(false);
+    cin.tie(0);
+    int n, k;
+    cin >> n >> k;
+    std::vector<int> A(n);
+    for(int i = 0; i < n; i++) {
+        cin >> A[i];
+    }
+    cout << kth(A, 0, n-1, k);
+}
