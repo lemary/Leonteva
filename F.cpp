fe4646a79fb6c9b83508301d141b5b7feb8e3e2e
lemary <69257731+lemary@users.noreply.github.com>
2021-03-20 19:02:57 +0300

    Add files via upload
    new commit

---
+++ F.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+//#include <vector>
+using std::cin;
+using std::cout;
+
+int main() {
+    
+    int size, size2;
+
+    cin >> size;
+    int mas[size + 1];
+    for (int i = 1; i < size + 1; ++i) {
+        cin >> mas[i];
+    }
+
+    cin >> size2;
+    int mas2[size2 + 1];
+    for (int i = 1; i < size2 + 1; ++i) {
+        cin >> mas2[i];
+    }
+
+    int ans[size + 1][size2 + 1];
+
+    // for (std::vector<int>& v : mas) { нежизнеспособно
+    //     for (int i : v) {
+    //         i = 0;
+    //     }
+    // }
+
+    for (int i = 0; i < size + 1; ++i) {
+        for (int k = 0; k < size2 + 1; ++k) {
+            ans[i][k] = 0;
+        }
+    }
+
+    for (int i = 1; i < size + 1; ++i) {
+        for (int k = 1; k < size2 + 1; ++k) {
+            if (mas[i] != mas2[k]) {
+                ans[i][k] = std::max(ans[i][k - 1], ans[i - 1][k]);
+            } else {
+                ans[i][k] = ans[i - 1][k - 1] + 1;
+            }
+        }
+    }
+
+    cout << ans[size][size2] << '\n';
+    return 0;
+}
