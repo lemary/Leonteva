87961bdd4683d8829f526f32336b5c0e5840a33d
lemary <69257731+lemary@users.noreply.github.com>
2021-06-28 00:05:57 +0300

    Update K.cpp

--- K.cpp
+++ K.cpp
@@ -1,44 +1,51 @@
-#include<iostream>
-#include<vector>
-#include<cmath>
+#include <iostream>
+using namespace std;
 
-using std::cin;
-using std::cout;
-
-int64_t krasava(size_t n, size_t m) { // имена функций должны быть осмысленными
-    std::ios::sync_with_stdio(false);
-    cin.tie(0);
-
-    std::vector <std::vector<size_t>> mas(n, std::vector<size_t>((1 << m), 0));
-    int64_t answer=0;
-
-    for (size_t a = 0; a < 1 << m; a++) {
-        mas[0][a] = 1;
-    }
-
-    for (size_t a = 1; a < n; a++) {
-        for (size_t b = 0; b < 1 << m; b++) {
-            for (size_t c = 0; c < 1 << m; c++) {
-                if ((((b & c) & ((c & b) >> 1)) == 0) && (((~b & ~c) & ((~c & ~b) >> 1)) % (1<<(m - 1)) == 0)) {  //jyj // очень длинное условие, которое вложно разобрать
-                    mas[a][c] = mas[a][c] + mas[a - 1][b];
-                }
-            }
-        }
-    }
-
-    for (size_t a = 0; a < pow(2, m); a++) {
-        answer = answer + mas[n - 1][a];
-    }
-
-    return(answer);
+bool Bit(long long musk, int p) {
+    if (((musk & (1 << p)) >> p) == 1) return true;
+    else return false;
 }
 
-int main() {
-    size_t n, m;
-    cin >> n >> m;
+bool IsSquareCorrect(long long musk_f, long long musk_s, int p) {
+    return !(Bit(musk_f, p) == Bit(musk_f, p + 1) &&
+            Bit(musk_f, p) == Bit(musk_s, p) &&
+            Bit(musk_s, p) == Bit(musk_s, p + 1));
+}
 
-    if (n < m) {
-        std::swap(n, m);
+bool correct(long long musk_f, long long musk_s, int m) {
+    for (auto k = 0; k < m - 1; k++)
+        if (!IsSquareCorrect(musk_f, musk_s, k)) return false;
+    return true;
+}
+
+
+int main() {
+    int n, m;
+    cin >> m >> n;
+    if (n > m) {
+        int c;
+        c = n;
+        n = m;
+        m = c;
     }
-    cout << krasava(n, m);
+
+    long long maska;
+    maska = ( 1 << n);
+    long long dp[maska][m];
+
+    for (auto l = 0; l < maska; l++)
+        for (auto k  = 0; k < m; k++) {
+            dp[l][k] = 0;
+            if (k == 0) dp[l][k] = 1;
+        }
+
+    for (auto k = 0; k < m - 1; k++)
+        for (auto musk_o = 0; musk_o < maska; musk_o++)
+            for (auto musk_n = 0; musk_n < maska; musk_n++)
+                if (correct(musk_o, musk_n, n)) dp[musk_n][k + 1] = dp[musk_n][k + 1] + dp[musk_o][k];
+
+    int ans = 0;
+    for (auto l = 0; l < maska; l++)
+        ans = ans + dp[l][m - 1];
+    cout << ans;
 }
